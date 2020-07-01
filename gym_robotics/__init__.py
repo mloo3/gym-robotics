6912e0ee9b8b1b0490bdcc2bf42824382d0c4c96
mloo3 <mloo3@gatech.edu>
2020-06-30 23:31:34 -0400

    added fetch assets and template from pickandplace

---
+++ gym_robotics/__init__.py
@@ -0,0 +1,14 @@
+from gym.envs.registration import register
+
+for reward_type in ['sparse', 'dense']:
+      suffix = 'Dense' if reward_type =='dense' else ''
+      kwargs = {
+            'reward_type': reward_type
+      }
+
+      register(
+            id='Test{}-v1'.format(suffix),
+            entry_point='gym_robotics.envs:FetchPickAndPlaceEnv',
+            kwargs=kwargs,
+            max_episode_steps=50,
+      )
---
+++ gym_robotics/envs/__init__.py
@@ -0,0 +1,3 @@
+from gym_robotics.envs.fetch_env import FetchEnv
+from gym_robotics.envs.fetch.pick_and_place import FetchPickAndPlaceEnv
+
