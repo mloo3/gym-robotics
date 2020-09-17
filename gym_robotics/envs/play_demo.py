df508314e33d6552347139e73f891b44d62a08c2
mloo3 <mloo3@gatech.edu>
2020-09-16 21:29:11 -0400

    play_demo

--- gym_robotics/envs/play_demo.py
+++ gym_robotics/envs/play_demo.py
@@ -1,18 +1,41 @@
 import numpy as np
 from gym.envs.robotics import rotations, robot_env, utils, fetch_env
 
+
 class PlayDemo(fetch_env.FetchEnv):
     def __init__(
-        self, model_path, n_substeps, gripper_extra_height, block_gripper,
-        has_object, target_in_the_air, target_offset, obj_range, target_range,
-        distance_threshold, initial_qpos, reward_type, goal_loc=None, obj_loc=None,
+        self,
+        model_path,
+        n_substeps,
+        gripper_extra_height,
+        block_gripper,
+        has_object,
+        target_in_the_air,
+        target_offset,
+        obj_range,
+        target_range,
+        distance_threshold,
+        initial_qpos,
+        reward_type,
+        goal_loc=None,
+        obj_loc=None,
     ):
         self.goal_loc = goal_loc
         self.obj_loc = obj_loc
         super(PlayDemo, self).__init__(
-            model_path, n_substeps, gripper_extra_height, block_gripper,
-            has_object, target_in_the_air, target_offset, obj_range, target_range,
-            distance_threshold, initial_qpos, reward_type)
+            model_path,
+            n_substeps,
+            gripper_extra_height,
+            block_gripper,
+            has_object,
+            target_in_the_air,
+            target_offset,
+            obj_range,
+            target_range,
+            distance_threshold,
+            initial_qpos,
+            reward_type,
+        )
 
     def _sample_goal(self):
         if self.goal_loc is None:
@@ -27,10 +50,10 @@ class PlayDemo(fetch_env.FetchEnv):
             self.sim.set_state(self.initial_state)
 
             if self.has_object:
-                object_qpos = self.sim.data.get_joint_qpos('object0:joint')
+                object_qpos = self.sim.data.get_joint_qpos("object0:joint")
                 assert object_qpos.shape == (7,)
                 object_qpos[:2] = self.obj_loc
-                self.sim.data.set_joint_qpos('object0:joint', object_qpos)
+                self.sim.data.set_joint_qpos("object0:joint", object_qpos)
 
             self.sim.forward()
             return True
@@ -40,6 +63,3 @@ class PlayDemo(fetch_env.FetchEnv):
 
     def set_obj_loc(self, obj_loc):
         self.obj_loc = obj_loc
-
-
-
